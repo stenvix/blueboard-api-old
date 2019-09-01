aa64c7d0b1036ec1b2ff95be3f26ebf09a0e27fb
Valentyn Stepanov <falken.ua@gmail.com>
2019-09-01 23:24:41 +0300

    Add get slim current user api

---
+++ src/BlueBoard.Application/Users/Models/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using BlueBoard.Domain;
+
+namespace BlueBoard.Application.Users.Models
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, SlimUserModel>();
+        }
+    }
+}
---
+++ src/BlueBoard.Application/Users/Queries/GetCurrentUser/GetCurrentUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using BlueBoard.Application.Users.Models;
+using MediatR;
+
+namespace BlueBoard.Application.Users.Queries.GetCurrentUser
+{
+    public class GetCurrentUserQuery : IRequest<SlimUserModel>
+    {
+    }
+}
