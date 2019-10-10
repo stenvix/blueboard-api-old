e92e73e4fc4a10ac2cf60ef1b374a9cdb0d078fd
Valentyn Stepanov <falken.ua@gmail.com>
2019-10-10 18:22:24 +0300

    Add username and avatar to participant model

--- src/BlueBoard.Application/Participants/Models/ParticipantModel.cs
+++ src/BlueBoard.Application/Participants/Models/ParticipantModel.cs
@@ -7,6 +7,8 @@ namespace BlueBoard.Application.Participants.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Avatar { get; set; }
         public ParticipantRole Role { get; set; }
     }
 }
--- src/BlueBoard.Application/Participants/Models/ParticipantProfile.cs
+++ src/BlueBoard.Application/Participants/Models/ParticipantProfile.cs
@@ -9,7 +9,9 @@ namespace BlueBoard.Application.Participants.Models
         {
             CreateMap<Participant, ParticipantModel>()
                 .ForMember(dest => dest.FirstName, src => src.MapFrom(i => i.User.FirstName))
-                .ForMember(dest => dest.LastName, src => src.MapFrom(i => i.User.LastName));
+                .ForMember(dest => dest.LastName, src => src.MapFrom(i => i.User.LastName))
+                .ForMember(dest => dest.Username, src => src.MapFrom(i => i.User.Username))
+                .ForMember(dest => dest.Avatar, src => src.MapFrom(i => i.User.Avatar));
         }
     }
 }
